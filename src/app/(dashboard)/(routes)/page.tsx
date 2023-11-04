21894b3b586bdf55ad3841012bf6025994a06338
anbehindY <yanminthwin@gmail.com>
2023-11-05 01:28:33 +0630

    Setup folder structure

---
+++ src/app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      <p className='text-center text-3xl p-5'>Protected Page</p>
+    </div>
+  )
+}
