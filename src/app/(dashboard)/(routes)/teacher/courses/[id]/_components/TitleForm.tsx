ad009daa3521deb9405678029ee188bb2c590995
anbehindY <yanminthwin@gmail.com>
2023-12-01 08:41:02 +0630

    Add title form

--- src/app/(dashboard)/(routes)/teacher/courses/[id]/_components/TitleForm.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[id]/_components/TitleForm.tsx
@@ -57,7 +57,7 @@ const TitleForm = ({
 
   return (
     <div className='mt-6 border bg-slate-100 rounded-md p-4'>
-      <div className='flex justify-between items-center font-medium'>
+      <div className='flex justify-between items-center font-medium mb-2'>
         Course Title
         <Button variant='ghost' onClick={() => setIsEditing(!isEditing)}>
           {isEditing ? 'Cancel' : <><Pencil className='h-4 w-4 mr-2' />
